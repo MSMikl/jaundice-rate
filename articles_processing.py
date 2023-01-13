c5f363ece4a79f7913f5d5ea63517c0bef347d3a
MSMikl <77015283+MSMikl@users.noreply.github.com>
2023-01-13 20:22:39 +0600

    Finalized

---
+++ articles_processing.py
@@ -0,0 +1,114 @@
+import aiohttp
+import asyncio
+import logging
+import os
+import time
+
+import pymorphy2
+
+from contextlib import asynccontextmanager
+from enum import Enum
+from urllib.parse import urlparse
+
+from adapters import SANITIZERS
+from adapters.exceptions import ArticleNotFound
+
+from text_tools import calculate_jaundice_rate, split_by_words
+
+
+TEST_ARTICLES = [
+    'https://inosmi.ru/not/exist.html',
+    'https://dvmn.org/modules/async-python/lesson/jaundice-rate/#9',
+    'https://inosmi.ru/20230113/energetika-259651741.html',
+]
+
+
+class ProcessingStatus(Enum):
+    OK = 'OK'
+    FETCHING_ERROR = 'FETCHING_ERROR'
+    TIMEOUT_ERROR = 'TIMEOUT_ERROR'
+    PARSING_ERROR = 'PARSING_ERROR'
+
+@asynccontextmanager
+async def timer():
+    time_start = time.monotonic()
+    def current_time():
+        return round(time.monotonic() - time_start, 3)
+    try:
+        yield current_time
+    finally:
+        pass
+
+
+async def fetch(session: aiohttp.ClientSession, url: str):
+    async with session.get(url) as response:
+        response.raise_for_status()
+        return await response.text()
+
+
+async def process_article(url: str, charged_words: list, morph: pymorphy2.MorphAnalyzer, timeout=3):
+    async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(timeout)) as session:
+        try:
+            html = await fetch(session, url)
+        except aiohttp.client_exceptions.ClientResponseError:
+            return url, ProcessingStatus('FETCHING_ERROR'), None, None, None
+        except asyncio.TimeoutError:
+            return url, ProcessingStatus('TIMEOUT_ERROR'), None, None, None
+        host = urlparse(url).netloc.replace('.', '_')
+        async with timer() as time_used:
+            try:
+                plaintext = await asyncio.to_thread(SANITIZERS[host], html, plaintext=True)
+            except (KeyError, ArticleNotFound):
+                return url, ProcessingStatus('PARSING_ERROR'), None, None, None
+            try:
+                async with asyncio.timeout(timeout): 
+                    splitted_text = await asyncio.to_thread(split_by_words, morph, plaintext)
+            except TimeoutError:
+                return url, ProcessingStatus('TIMEOUT_ERROR'), None, None, time_used()
+            analyzing_time = time_used()
+        result = url, ProcessingStatus('OK'), len(splitted_text), calculate_jaundice_rate(splitted_text, charged_words), analyzing_time
+        return result
+
+
+async def main():
+    logging.basicConfig(level=logging.INFO)
+    with open(os.path.join('.', 'data', 'negative_words.txt'), 'r', encoding='UTF-8') as file:
+        charged_words = [readline.strip() for readline in file]
+    morph = pymorphy2.MorphAnalyzer()
+    results = []
+    async with asyncio.TaskGroup() as tg:
+        for url in TEST_ARTICLES:
+            result = tg.create_task(process_article(url, charged_words, morph))
+            results.append(result)
+    for result in results:
+        url, status, words_count, rating, time_used = result.result()
+        print('URL', url)
+        print('Статус', status.name)
+        print('Количество слов:', words_count)
+        print('Рейтинг желтушности:', rating)
+        logging.info(f'Время на анализ составило {time_used} секунд')
+        print('\n')
+
+
+def test_process_article():
+    with open(os.path.join('.', 'data', 'negative_words.txt'), 'r', encoding='UTF-8') as file:
+        charged_words = [readline.strip() for readline in file]
+    morph = pymorphy2.MorphAnalyzer()
+    results = []
+    for url in TEST_ARTICLES:
+        results.append(asyncio.run(process_article(url, charged_words, morph)))
+    assert results[0] == (TEST_ARTICLES[0], ProcessingStatus('FETCHING_ERROR'), None, None, None)
+    assert results[1] == (TEST_ARTICLES[1], ProcessingStatus('PARSING_ERROR'), None, None, None)
+    assert results[2][0] == TEST_ARTICLES[2]
+    assert results[2][1] == ProcessingStatus('OK')
+    assert results[2][2] > 0
+    assert 0.0 < results[2][3] < 100.0
+
+    timeout_result = asyncio.run(process_article(TEST_ARTICLES[2], charged_words, morph, timeout=0.3))
+    assert timeout_result[1] == ProcessingStatus('TIMEOUT_ERROR')
+
+
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
---
+++ server.py
@@ -0,0 +1,45 @@
+import asyncio
+import os
+
+import aiohttp
+import pymorphy2
+
+from aiohttp import web
+from functools import partial
+
+from articles_processing import process_article
+
+
+async def analyze_page(request: aiohttp.ClientRequest, charged_words: list, morph: pymorphy2.MorphAnalyzer):
+    params = request.rel_url.query.get('urls')
+    if not params:
+        return web.json_response({})
+    if len(params) > 10:
+        return web.json_response({'error': "too many urls in request, should be 10 or less"}, status=400)
+    urls = params.split(',')
+    tasks = []
+    async with asyncio.TaskGroup() as tg:
+        for url in urls:
+            task = tg.create_task(process_article(url, charged_words, morph))
+            tasks.append(task)
+    response_data = []
+    for task in tasks:
+        result = task.result()
+        response_data.append(
+            {
+                'url': result[0],
+                'status': result[1].name,
+                'score': result[3],
+                'words_count': result[2],
+            }
+        )
+    return web.json_response(response_data)
+
+
+with open(os.path.join('.', 'data', 'negative_words.txt'), 'r', encoding='UTF-8') as file:
+    charged_words = [readline.strip() for readline in file]
+morph = pymorphy2.MorphAnalyzer()
+callback = partial(analyze_page, charged_words=charged_words, morph=morph)
+app = web.Application()
+app.add_routes([web.get('/', callback)])
+web.run_app(app)
